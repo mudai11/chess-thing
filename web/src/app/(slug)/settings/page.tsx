fa601ccf6e5e0c45f5133350fc6390953d4b32b4
akatheos <akatheos11@gmail.com>
2023-11-04 19:52:32 +0100

    small

--- web/src/app/(slug)/settings/page.tsx
+++ web/src/app/(slug)/settings/page.tsx
@@ -13,7 +13,6 @@ export default async function Settings() {
     <div className="max-w-4xl mx-auto py-12">
       <div className="grid items-start gap-8">
         <h1 className="font-bold text-3xl md:text-4xl">Settings</h1>
-
         <div className="grid gap-5">
           <UserNameForm
             user={{
