bace0159961be781c69420536dd342955ebcf742
akatheos <akatheos11@gmail.com>
2023-11-19 09:30:12 +0100

    fixed eslint && added docker in the server && node version in packagejson

--- web/next.config.js
+++ web/next.config.js
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
   output: "standalone",
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
