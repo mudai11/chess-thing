05ce5b450c9fc7779e69e382466661fa145ea2ef
akatheos <akatheos11@gmail.com>
2023-11-19 12:22:11 +0100

    wooork

--- server/src/modules/user/user.controller.ts
+++ server/src/modules/user/user.controller.ts
@@ -41,7 +41,7 @@ async function createUserHandler(
         secure: true,
         maxAge: 10 * 24 * 60 * 60,
         sameSite: "none",
-        domain: ".chess-thing-alpha.vercel.app",
+        domain: "chess-thing-server.onrender.com",
       })
       .send({
         message: "Success",
@@ -104,7 +104,7 @@ async function signinUserHandler(
         secure: true,
         maxAge: 10 * 24 * 60 * 60,
         sameSite: "none",
-        domain: ".chess-thing-alpha.vercel.app",
+        domain: "chess-thing-server.onrender.com",
       })
       .send({ message: "Success" });
   } catch (e) {
@@ -323,7 +323,7 @@ async function deleteSessionHandler(
         secure: true,
         maxAge: 0,
         sameSite: "none",
-        domain: ".chess-thing-alpha.vercel.app",
+        domain: "chess-thing-server.onrender.com",
       })
       .send({ message: "Success" });
   } catch (e) {
